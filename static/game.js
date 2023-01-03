c48d3b77db8d118a6566c6ce31beb4ee1711cf2d
adiouf <h8799r@gmail.com>
2023-01-03 16:01:29 +0100

    Ajout de l'animation de typing

--- static/game.js
+++ static/game.js
@@ -1,4 +1,7 @@
+proposed = false
+
 function onProposalClick(proposal) {
+    proposed = true
     quote = sessionStorage.getItem("quote")
     score = sessionStorage.getItem("score") || "0"
     quoteNb = sessionStorage.getItem("quoteNb") || "1"
@@ -10,11 +13,12 @@ function onProposalClick(proposal) {
             quoteNb = parseInt(data.quoteNb)
             storeItem("quoteNb", quoteNb)
             message = (data.correct) ? "Bravo ! Ton score passe à " : "Et non c'était '" + data.answer + "', ton score reste à "
-            message += score + ". (il reste " + (10 - quoteNb).toString() + " tour(s))"
+            message += score + " (il reste " + (10 - quoteNb).toString() + " tour(s))"
             document.querySelector("#random_quote").innerHTML = message
             goToNextMessage = "<li onclick=\"getNewQuote()\">Suivant</li>"
             endMessage = "<a href=\"end.html\">Fin</a>"
             document.querySelector("#proposals").innerHTML = (quoteNb == 10) ? endMessage : goToNextMessage
+            proposed = false
         })
 }
 
@@ -40,7 +44,8 @@ function getNewQuote() {
     fetch('/random')
         .then((response) => response.json())
         .then((data) => {
-            document.querySelector('#random_quote').textContent = data.quote
+            document.querySelector('#random_quote').textContent = ""
+            typeWriter(data.quote, "random_quote")
             storeItem("quote", data.id)
             setNewProposition(data.movie)
             document.querySelector("#proposals").innerHTML = ""
@@ -49,6 +54,15 @@ function getNewQuote() {
 
 function storeItem(key, value) {
     sessionStorage.setItem(key, value);
+}
+
+function typeWriter(txt, id, i = 0) {
+    speed = 30
+    if (i < txt.length) {
+      document.getElementById(id).innerHTML += txt.charAt(i);
+      i++;
+      if(!proposed) setTimeout(() => {typeWriter(txt, id, i)}, speed);
+    }
 }
 
 getNewQuote()
